5f74031162fbe445abc4c8f3552b6f4d3ca5fd33
Tanja <tavinogradova92@gmail.com>
2020-01-26 11:52:25 +0100

    The main components base created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SideNavigationComponent } from './sidenav/side-navigation/side-navigation.component';
+import { SearchBarComponent } from './home/search/search-bar/search-bar.component';
+import { ItemsListComponent } from './home/items-list/items-list/items-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, SideNavigationComponent, SearchBarComponent, ItemsListComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
