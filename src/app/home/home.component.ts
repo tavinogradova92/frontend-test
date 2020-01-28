61e6a2f642ac3f53c4b01173fbc2a8d17f4e297d
Tanja <tavinogradova92@gmail.com>
2020-01-28 23:13:29 +0100

    Tree structure created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemDataService } from '../../item-data.service';
 import { Item } from '../../interfaces/item';
+declare var require: any;
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,44 @@ export class HomeComponent implements OnInit {
   
   items;
 
+  tree_items:Item;
+
   constructor(private listOfItems: ItemDataService) { }
 
   ngOnInit() {
+
+    // npm list-to-tree used, credentials: https://www.npmjs.com/package/list-to-tree
+    let LTT = require('list-to-tree');
+
     this.listOfItems.getItems()
-      .subscribe(res => this.items = res)
+      .subscribe(res => {
+        for (var i in res) {
+          res[i].parent_id = res[i].parent_id || 0; //replacing 'null' with 0 because of this npm requirenments
+        }
+
+        let ltt = new LTT(res, {
+          key_id: 'id',
+          key_parent: 'parent_id'
+           });
+
+        let tree = ltt.GetTree(); //npm method to create tree data structure from the list 
+   
+      // function which makes tree structure flat again in a right sequential order with depth calculation
+        function traverse_tree(t,depth) {
+          for (var i in t) {
+            tree_items.push({  'id'    : t[i].id,
+                               'title' : t[i].title,
+                               'depth' : depth});
+
+            if (t[i].child !== null ) {
+                traverse_tree(t[i].child,depth+1);
+              }
+          }
+    }
+      let tree_items = [];
+      traverse_tree(tree,0);
+      this.items = tree_items;
+     
+      })
   }
 }
--- src/interfaces/item.ts
+++ src/interfaces/item.ts
@@ -1,6 +1,5 @@
 export interface Item {
     id: number;
     title: string;
-    parent_id: number;
-    children?: Item[];
+    depth: number;
   }
