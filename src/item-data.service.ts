9d1789eba9bd77bc4502bbe7bb076638f332e20c
Tanja <tavinogradova92@gmail.com>
2020-01-28 23:28:52 +0100

    Error handler removed

--- src/item-data.service.ts
+++ src/item-data.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Item } from './interfaces/item';
-import { catchError, map } from 'rxjs/operators';
-import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,19 +12,7 @@ export class ItemDataService {
 
   constructor(private http: HttpClient) { }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error('client error:', error.error.message);
-    } else {
-      console.error('server error:', `${error.status},${error.error}`);
-    }
-    return throwError('Ooops, you have an error');
-  }
-
   getItems() {
     return this.http.get<Item>(this.url)
-    .pipe(
-      catchError(this.handleError)
-    );
   }
 }
